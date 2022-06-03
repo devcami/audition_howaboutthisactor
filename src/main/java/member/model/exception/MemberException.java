85ec005437b06abcc41d85c9411a60269eb07062
skygirl20 <parkmin100@naver.com>
2022-06-03 23:05:20 +0900

    Member 패키지 작성

---
+++ src/main/java/member/model/exception/MemberException.java
@@ -0,0 +1,30 @@
+package member.model.exception;
+
+public class MemberException extends RuntimeException {
+
+	public MemberException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public MemberException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+	public MemberException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public MemberException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public MemberException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+}
