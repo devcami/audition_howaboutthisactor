5ce61227f6136e58d30173c9f7e03fc335671ee3
devcami <juf1015@gmail.com>
2022-06-11 15:09:37 +0900

    wishlist package

---
+++ src/main/java/wishlist/model/exception/WishListException.java
@@ -0,0 +1,30 @@
+package wishlist.model.exception;
+
+public class WishListException extends RuntimeException {
+
+	public WishListException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public WishListException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+	public WishListException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public WishListException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public WishListException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+	
+}
