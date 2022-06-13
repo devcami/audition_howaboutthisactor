bd3c598cd63ea1be00962153753e0150ae095227
jess0402 <jyjmjs2@naver.com>
2022-06-13 12:25:21 +0900

    commit

--- src/main/java/wishlist/model/service/WishListService.java
+++ src/main/java/wishlist/model/service/WishListService.java
@@ -4,6 +4,7 @@ import wishlist.model.dao.WishListDao;
 
 public class WishListService {
 	private WishListDao wishListDao = new WishListDao();
+	public static final int NUM_PER_PAGE = 8;
 
 	/**
 	 * 은성부분
