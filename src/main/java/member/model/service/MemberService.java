94d7097c8a5ad8ed5d460d56f95504d5962ef2b9
skygirl20 <parkmin100@naver.com>
2022-06-14 23:17:09 +0900

    회원가입 폼 멤버 멤버 수정/ 서비스 필요없는 부분 제거

--- src/main/java/member/model/service/MemberService.java
+++ src/main/java/member/model/service/MemberService.java
@@ -39,58 +39,6 @@ public class MemberService {
 		return result;
 	}
 
-	public int updateMember(Member member) {
-		int result = 0;
-		// 1. Connection객체 생성
-		Connection conn = getConnection();
-		try {
-			// 2. dao 요청
-			result = memberDao.updateMember(conn, member);
-			// 3. 트랜잭션 처리
-			commit(conn);
-		} catch (Exception e) {
-			rollback(conn);
-			throw e; // controller 통보용
-		} finally {
-			// 4. Connection객체 반환
-			close(conn);
-		}
-		return result;
-	}
-
-	public int deleteMember(String memberId) {
-		int result = 0;
-		// 1. Connection객체 생성
-		Connection conn = getConnection();
-		try {
-			// 2. dao 요청
-			result = memberDao.deleteMember(conn, memberId);
-			// 3. 트랜잭션 처리
-			commit(conn);
-		} catch (Exception e) {
-			rollback(conn);
-			throw e; // controller 통보용
-		} finally {
-			// 4. Connection객체 반환
-			close(conn);
-		}
-		return result;
-	}
-
-	public int updatePassword(Member member) {
-		int result = 0;
-		Connection conn = getConnection();
-		try {
-			result = memberDao.updatePassword(conn, member);
-			commit(conn);
-		} catch (Exception e) {
-			rollback(conn);
-			throw e;
-		} finally {
-			close(conn);
-		}
-		return result;
-	}
 	
 	public List<Member> findAll(Map<String, Object> param) {
 		Connection conn = getConnection();
--- src/main/webapp/WEB-INF/views/member/memberEnroll.jsp
+++ src/main/webapp/WEB-INF/views/member/memberEnroll.jsp
@@ -2,6 +2,10 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
 	pageEncoding="UTF-8"%>
 
+<%@page import="java.sql.Date"%>
+<%@page import="member.model.dto.Member"%>	
+<%@page import="member.model.dto.Production" %>
+<%@page import="ann.model.dto.Ann"%>	
 <%@ include file="/WEB-INF/views/common/header.jsp" %>
 <link rel="stylesheet" href="<%= request.getContextPath() %>/css/mEnroll.css" />
 <section id=enroll-container>
